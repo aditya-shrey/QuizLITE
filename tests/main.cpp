b08fd70367398472cc854617a87d174d188b664a
A. Shrey <aditya.shrey4@gmail.com>
2024-06-29 01:46:35 -0700

    Add tests and linter

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Aditya Shrey on 6/28/24.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    // Run test cases.
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/testDatabase.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Aditya Shrey on 6/28/24.
+//
+
+#include <gtest/gtest.h>
+
+TEST(DatabaseTest, DummyTest) {
+    EXPECT_EQ(1, 1);
+}
