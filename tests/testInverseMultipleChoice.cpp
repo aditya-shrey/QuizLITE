b21e4ae0deeacded1ff4b07ecc5fb59c37ec5292
A. Shrey <aditya.shrey4@gmail.com>
2024-07-12 02:37:32 -0700

    Remove test from Inverse MC pertainng to test generate outputs unreliable checking

--- tests/testInverseMultipleChoice.cpp
+++ tests/testInverseMultipleChoice.cpp
@@ -48,35 +48,12 @@ TEST_F(InverseMultipleChoiceTest, TestGetQuestionAndAnswer)
     for (int i = 0; i < 3; ++i) {
         std::string question = inverseMultipleChoice.getQuestion();
         std::string answer = inverseMultipleChoice.getAnswer();
-        std::cout << "Question: " << question << ", Answer: " << answer << std::endl;
         ASSERT_TRUE(expectedQuestions.find(answer) != expectedQuestions.end());
         ASSERT_TRUE(expectedAnswers.find(question) != expectedAnswers.end());
         expectedQuestions.erase(question);
         expectedAnswers.erase(answer);
         inverseMultipleChoice.goToNextQuestion();
     }
-}
-
-TEST_F(InverseMultipleChoiceTest, TestGenerateOptions)
-{
-    TestableInverseMultipleChoice inverseMultipleChoice("History", 3, 0); // 3 lowest accuracy entries, no random entries
-
-    // Test options for the first answer
-    auto options = inverseMultipleChoice.generateOptions();
-    ASSERT_TRUE(
-        std::get<0>(options) == "1776" || std::get<1>(options) == "1776" || std::get<2>(options) == "1776" || std::get<3>(options) == "1776");
-
-    // Test options for the second answer
-    inverseMultipleChoice.goToNextQuestion();
-    options = inverseMultipleChoice.generateOptions();
-    ASSERT_TRUE(
-        std::get<0>(options) == "1945" || std::get<1>(options) == "1945" || std::get<2>(options) == "1945" || std::get<3>(options) == "1945");
-
-    // Test options for the third answer
-    inverseMultipleChoice.goToNextQuestion();
-    options = inverseMultipleChoice.generateOptions();
-    ASSERT_TRUE(
-        std::get<0>(options) == "1969" || std::get<1>(options) == "1969" || std::get<2>(options) == "1969" || std::get<3>(options) == "1969");
 }
 
 TEST_F(InverseMultipleChoiceTest, TestUpdateScoresAndOrder)
