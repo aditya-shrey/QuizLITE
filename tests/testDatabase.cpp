484c89d9cf6a48b2d4da3e56df66b9a61fd34ae2
fbablu <fardeeneb@gmail.com>
2024-07-18 15:58:09 -0400

    For Linter

--- tests/testDatabase.cpp
+++ tests/testDatabase.cpp
@@ -3,72 +3,65 @@
 
 class DatabaseManagerTest : public ::testing::Test {
 protected:
-    void SetUp() override
-    {
+  void SetUp() override {
 #ifdef TESTING
-        DatabaseManager::resetInstance();
-        dbManager = DatabaseManager::getDatabaseManager(":memory:");
-        dbManager->openDatabase();
+    DatabaseManager::resetInstance();
+    dbManager = DatabaseManager::getDatabaseManager(":memory:");
+    dbManager->openDatabase();
 #endif
-    }
-    void TearDown() override
-    {
+  }
+  void TearDown() override {
 #ifdef TESTING
-        dbManager->closeDatabase();
-        DatabaseManager::resetInstance();
-        unsetenv("TEST_ENV");
+    dbManager->closeDatabase();
+    DatabaseManager::resetInstance();
+    unsetenv("TEST_ENV");
 #endif
-    }
+  }
 
-    DatabaseManager* dbManager {};
+  DatabaseManager *dbManager{};
 };
 
-TEST_F(DatabaseManagerTest, SingletonInstance)
-{
-    DatabaseManager* dbManager1 = DatabaseManager::getDatabaseManager(":memory:");
-    DatabaseManager* dbManager2 = DatabaseManager::getDatabaseManager(":memory:");
-    EXPECT_EQ(dbManager1, dbManager2);
+TEST_F(DatabaseManagerTest, SingletonInstance) {
+  DatabaseManager *dbManager1 = DatabaseManager::getDatabaseManager(":memory:");
+  DatabaseManager *dbManager2 = DatabaseManager::getDatabaseManager(":memory:");
+  EXPECT_EQ(dbManager1, dbManager2);
 }
 
-TEST_F(DatabaseManagerTest, OpenDatabase)
-{
-    EXPECT_TRUE(dbManager->openDatabase());
+TEST_F(DatabaseManagerTest, OpenDatabase) {
+  EXPECT_TRUE(dbManager->openDatabase());
 }
 
-TEST_F(DatabaseManagerTest, CloseDatabase)
-{
-    dbManager->closeDatabase();
-    dbManager->closeDatabase(); // This is just for testing multiple close calls
+TEST_F(DatabaseManagerTest, CloseDatabase) {
+  dbManager->closeDatabase();
+  dbManager->closeDatabase(); // This is just for testing multiple close calls
 }
 
-TEST_F(DatabaseManagerTest, ExecuteQuery)
-{
-    EXPECT_EQ(dbManager->executeQuery("CREATE TABLE test (name TEXT);"),
-        SQLITE_OK);
-    EXPECT_EQ(
-        dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');"),
-        SQLITE_OK);
-    EXPECT_EQ(dbManager->executeQuery("INSERT INTO test (name) VALUES ('Bob');"),
-        SQLITE_OK);
+TEST_F(DatabaseManagerTest, ExecuteQuery) {
+  EXPECT_EQ(dbManager->executeQuery("CREATE TABLE test (name TEXT);"),
+            SQLITE_OK);
+  EXPECT_EQ(
+      dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');"),
+      SQLITE_OK);
+  EXPECT_EQ(dbManager->executeQuery("INSERT INTO test (name) VALUES ('Bob');"),
+            SQLITE_OK);
 }
 
-TEST_F(DatabaseManagerTest, ExecuteQueryWithResults)
-{
-    dbManager->executeQuery("CREATE TABLE test (name TEXT);");
-    dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');");
-    dbManager->executeQuery("INSERT INTO test (name) VALUES ('Bob');");
+TEST_F(DatabaseManagerTest, ExecuteQueryWithResults) {
+  dbManager->executeQuery("CREATE TABLE test (name TEXT);");
+  dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');");
+  dbManager->executeQuery("INSERT INTO test (name) VALUES ('Bob');");
 
-    std::vector<std::map<std::string, std::string>> results = dbManager->executeQueryWithResults("SELECT * FROM test;");
-    ASSERT_EQ(results.size(), 2);
-    EXPECT_EQ(results[0]["name"], "Alice");
-    EXPECT_EQ(results[1]["name"], "Bob");
+  std::vector<std::map<std::string, std::string>> results =
+      dbManager->executeQueryWithResults("SELECT * FROM test;");
+  ASSERT_EQ(results.size(), 2);
+  EXPECT_EQ(results[0]["name"], "Alice");
+  EXPECT_EQ(results[1]["name"], "Bob");
 }
 
-TEST_F(DatabaseManagerTest, IsTableEmpty)
-{
-    dbManager->executeQuery("CREATE TABLE test (name TEXT);");
-    EXPECT_TRUE(dbManager->isTableEmpty("test"));
+TEST_F(DatabaseManagerTest, IsTableEmpty) {
+  dbManager->executeQuery("CREATE TABLE test (name TEXT);");
+  EXPECT_TRUE(dbManager->isTableEmpty("test"));
 
-    dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');");
-    EXPECT_FALSE(dbManager->isTableEmpty("test"));
+  dbManager->executeQuery("INSERT INTO test (name) VALUES ('Alice');");
+  EXPECT_FALSE(dbManager->isTableEmpty("test"));
 }
--- tests/testUser.cpp
+++ tests/testUser.cpp
@@ -5,135 +5,118 @@
 
 class CoutRedirect {
 public:
-    CoutRedirect(std::streambuf* new_buffer)
-        : old(std::cout.rdbuf(new_buffer))
-    {
-    }
-    ~CoutRedirect() { std::cout.rdbuf(old); }
+  CoutRedirect(std::streambuf *new_buffer) : old(std::cout.rdbuf(new_buffer)) {}
+  ~CoutRedirect() { std::cout.rdbuf(old); }
 
 private:
-    std::streambuf* old;
+  std::streambuf *old;
 };
 
 class UserSessionTest : public ::testing::Test {
 protected:
-    void SetUp() override
-    {
+  void SetUp() override {
 #ifdef TESTING
-        UserSession::resetInstance();
-        userSession = UserSession::getUserSession();
+    UserSession::resetInstance();
+    userSession = UserSession::getUserSession();
 #endif
-    }
+  }
 
-    void TearDown() override
-    {
+  void TearDown() override {
 #ifdef TESTING
-        UserSession::resetInstance();
-        unsetenv("TEST_ENV");
+    UserSession::resetInstance();
+    unsetenv("TEST_ENV");
 #endif
-    }
+  }
 
-    UserSession* userSession {};
+  UserSession *userSession{};
 };
 
-TEST_F(UserSessionTest, SingletonTest)
-{
-    UserSession* userSession1 = UserSession::getUserSession();
-    UserSession* userSession2 = UserSession::getUserSession();
+TEST_F(UserSessionTest, SingletonTest) {
+  UserSession *userSession1 = UserSession::getUserSession();
+  UserSession *userSession2 = UserSession::getUserSession();
 
-    EXPECT_EQ(userSession1, userSession2);
+  EXPECT_EQ(userSession1, userSession2);
 }
 
-TEST_F(UserSessionTest, CreateAndCheckStudySet)
-{
-    EXPECT_TRUE(userSession->createStudySet("NewSet"));
-    EXPECT_TRUE(userSession->existsStudySet("NewSet"));
+TEST_F(UserSessionTest, CreateAndCheckStudySet) {
+  EXPECT_TRUE(userSession->createStudySet("NewSet"));
+  EXPECT_TRUE(userSession->existsStudySet("NewSet"));
 }
 
-TEST_F(UserSessionTest, DeleteStudySet)
-{
-    userSession->createStudySet("SetToDelete");
-    EXPECT_TRUE(userSession->deleteStudySet("SetToDelete"));
-    EXPECT_FALSE(userSession->existsStudySet("SetToDelete"));
+TEST_F(UserSessionTest, DeleteStudySet) {
+  userSession->createStudySet("SetToDelete");
+  EXPECT_TRUE(userSession->deleteStudySet("SetToDelete"));
+  EXPECT_FALSE(userSession->existsStudySet("SetToDelete"));
 }
 
-TEST_F(UserSessionTest, AddAndDeleteFromStudySet)
-{
-    userSession->createStudySet("SetForAddDelete");
-    EXPECT_TRUE(userSession->addToStudySet("SetForAddDelete", "Key1", "Value1"));
-    EXPECT_TRUE(userSession->deleteFromStudySet("SetForAddDelete", "Key1"));
+TEST_F(UserSessionTest, AddAndDeleteFromStudySet) {
+  userSession->createStudySet("SetForAddDelete");
+  EXPECT_TRUE(userSession->addToStudySet("SetForAddDelete", "Key1", "Value1"));
+  EXPECT_TRUE(userSession->deleteFromStudySet("SetForAddDelete", "Key1"));
 }
 
-TEST_F(UserSessionTest, IsSetNamesTableEmpty)
-{
-    EXPECT_TRUE(userSession->emptyAllSets());
-    EXPECT_TRUE(userSession->isSetNamesTableEmpty());
-    userSession->createStudySet("NonEmptySet");
-    EXPECT_FALSE(userSession->isSetNamesTableEmpty());
+TEST_F(UserSessionTest, IsSetNamesTableEmpty) {
+  EXPECT_TRUE(userSession->emptyAllSets());
+  EXPECT_TRUE(userSession->isSetNamesTableEmpty());
+  userSession->createStudySet("NonEmptySet");
+  EXPECT_FALSE(userSession->isSetNamesTableEmpty());
 }
 
-TEST_F(UserSessionTest, UpdateScore)
-{
-    userSession->createStudySet("ScoreSet");
-    userSession->addToStudySet("ScoreSet", "Key1", "Value1");
-    EXPECT_TRUE(userSession->updateScore("ScoreSet", "Key1", true));
+TEST_F(UserSessionTest, UpdateScore) {
+  userSession->createStudySet("ScoreSet");
+  userSession->addToStudySet("ScoreSet", "Key1", "Value1");
+  EXPECT_TRUE(userSession->updateScore("ScoreSet", "Key1", true));
 }
 
-TEST_F(UserSessionTest, GetTableKeyValues)
-{
-    userSession->createStudySet("KeyValueSet");
-    userSession->addToStudySet("KeyValueSet", "Key1", "Value1");
-    auto keyValues = userSession->getTableKeyValues("KeyValueSet");
-    ASSERT_EQ(keyValues.size(), 1);
-    EXPECT_EQ(keyValues[0].first, "Key1");
-    EXPECT_EQ(keyValues[0].second, "Value1");
+TEST_F(UserSessionTest, GetTableKeyValues) {
+  userSession->createStudySet("KeyValueSet");
+  userSession->addToStudySet("KeyValueSet", "Key1", "Value1");
+  auto keyValues = userSession->getTableKeyValues("KeyValueSet");
+  ASSERT_EQ(keyValues.size(), 1);
+  EXPECT_EQ(keyValues[0].first, "Key1");
+  EXPECT_EQ(keyValues[0].second, "Value1");
 }
 
-TEST_F(UserSessionTest, GetLowestAccuracies)
-{
-    userSession->createStudySet("AccuracySet");
-    userSession->addToStudySet("AccuracySet", "Key1", "Value1");
-    auto accuracies = userSession->getLowestAccuracies("AccuracySet", 1);
-    ASSERT_EQ(accuracies.size(), 1);
-    EXPECT_EQ(std::get<0>(accuracies[0]), "Key1");
+TEST_F(UserSessionTest, GetLowestAccuracies) {
+  userSession->createStudySet("AccuracySet");
+  userSession->addToStudySet("AccuracySet", "Key1", "Value1");
+  auto accuracies = userSession->getLowestAccuracies("AccuracySet", 1);
+  ASSERT_EQ(accuracies.size(), 1);
+  EXPECT_EQ(std::get<0>(accuracies[0]), "Key1");
 }
 
-TEST_F(UserSessionTest, GetRandomEntries)
-{
-    userSession->createStudySet("RandomSet");
-    userSession->addToStudySet("RandomSet", "Key1", "Value1");
-    auto randomEntries = userSession->getRandomEntries("RandomSet", 1);
-    ASSERT_EQ(randomEntries.size(), 1);
-    EXPECT_EQ(randomEntries[0].first, "Key1");
+TEST_F(UserSessionTest, GetRandomEntries) {
+  userSession->createStudySet("RandomSet");
+  userSession->addToStudySet("RandomSet", "Key1", "Value1");
+  auto randomEntries = userSession->getRandomEntries("RandomSet", 1);
+  ASSERT_EQ(randomEntries.size(), 1);
+  EXPECT_EQ(randomEntries[0].first, "Key1");
 }
 
-TEST_F(UserSessionTest, EmptyAllSets)
-{
-    userSession->createStudySet("SetToEmpty");
-    EXPECT_TRUE(userSession->emptyAllSets());
-    EXPECT_TRUE(userSession->isSetNamesTableEmpty());
+TEST_F(UserSessionTest, EmptyAllSets) {
+  userSession->createStudySet("SetToEmpty");
+  EXPECT_TRUE(userSession->emptyAllSets());
+  EXPECT_TRUE(userSession->isSetNamesTableEmpty());
 }
 
-TEST_F(UserSessionTest, PrintDatabaseTable)
-{
-    userSession->createStudySet("PrintSet");
-    userSession->addToStudySet("PrintSet", "Key1", "Value1");
+TEST_F(UserSessionTest, PrintDatabaseTable) {
+  userSession->createStudySet("PrintSet");
+  userSession->addToStudySet("PrintSet", "Key1", "Value1");
 
-    std::ostringstream oss;
-    CoutRedirect redirect(oss.rdbuf());
-    userSession->printDatabaseTable("PrintSet");
+  std::ostringstream oss;
+  CoutRedirect redirect(oss.rdbuf());
+  userSession->printDatabaseTable("PrintSet");
 
-    std::string output = oss.str();
-    EXPECT_NE(output.find("Key1"), std::string::npos);
-    EXPECT_NE(output.find("Value1"), std::string::npos);
+  std::string output = oss.str();
+  EXPECT_NE(output.find("Key1"), std::string::npos);
+  EXPECT_NE(output.find("Value1"), std::string::npos);
 }
 
-TEST_F(UserSessionTest, GetTable)
-{
-    userSession->createStudySet("TableSet");
-    userSession->addToStudySet("TableSet", "Key1", "Value1");
-    auto tableData = userSession->getTable("TableSet");
-    ASSERT_EQ(tableData.size(), 1);
-    EXPECT_EQ(std::get<1>(tableData[0]), "Key1");
-    EXPECT_EQ(std::get<2>(tableData[0]), "Value1");
+TEST_F(UserSessionTest, GetTable) {
+  userSession->createStudySet("TableSet");
+  userSession->addToStudySet("TableSet", "Key1", "Value1");
+  auto tableData = userSession->getTable("TableSet");
+  ASSERT_EQ(tableData.size(), 1);
+  EXPECT_EQ(std::get<1>(tableData[0]), "Key1");
+  EXPECT_EQ(std::get<2>(tableData[0]), "Value1");
 }
