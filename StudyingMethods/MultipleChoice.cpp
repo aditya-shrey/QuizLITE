e75e6de29be4f05d0d2ae696553980d0f6b90a33
A. Shrey <aditya.shrey4@gmail.com>
2024-08-01 01:01:15 -0700

    Fix duplicate options and questions in MultipleChoice

--- StudyingMethods/MultipleChoice.cpp
+++ StudyingMethods/MultipleChoice.cpp
@@ -1,18 +1,35 @@
 #include "MultipleChoice.h"
+#include <unordered_set>
+#include <random>
+#include <algorithm>
+
+// Hash function for std::pair to be used in std::unordered_set
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2>& pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
 
 MultipleChoice::MultipleChoice(const std::string& setName, int numLowestAccuracies, int numRandomEntries)
-    : currentIndex(0)
-    , setName(setName)
+        : currentIndex(0)
+        , setName(setName)
 {
     auto userSession = UserSession::getUserSession();
 
+    std::unordered_set<std::pair<std::string, std::string>, pair_hash> uniqueEntries;
+
     auto lowestAccuracies = userSession->getLowestAccuracies(setName, numLowestAccuracies);
     for (const auto& entry : lowestAccuracies) {
-        keyValues.emplace_back(std::get<0>(entry), std::get<1>(entry));
+        uniqueEntries.insert(std::make_pair(std::get<0>(entry), std::get<1>(entry)));
     }
 
     auto randomEntries = userSession->getRandomEntries(setName, numRandomEntries);
-    keyValues.insert(keyValues.end(), randomEntries.begin(), randomEntries.end());
+    for (const auto& entry : randomEntries) {
+        uniqueEntries.insert(entry);
+    }
+
+    keyValues.assign(uniqueEntries.begin(), uniqueEntries.end());
 
     std::shuffle(keyValues.begin(), keyValues.end(), std::mt19937 { std::random_device {}() });
 }
@@ -45,12 +62,29 @@ bool MultipleChoice::goToNextQuestion()
 std::tuple<std::string, std::string, std::string, std::string> MultipleChoice::generateOptions()
 {
     std::vector<std::pair<std::string, std::string>> options;
-    std::sample(keyValues.begin(), keyValues.end(), std::back_inserter(options),
-        4, std::mt19937 { std::random_device {}() });
-    if (std::find(options.begin(), options.end(), keyValues[currentIndex]) == options.end()) {
-        options[0] = keyValues[currentIndex]; // Ensure the correct answer is one of the options
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    if (keyValues.size() <= 4) {
+        options = keyValues;
+    } else {
+        std::unordered_set<std::pair<std::string, std::string>, pair_hash> optionSet;
+        while (optionSet.size() < 3) {
+            std::uniform_int_distribution<> dis(0, keyValues.size() - 1);
+            auto sampled = keyValues[dis(gen)];
+            if (sampled != keyValues[currentIndex]) {
+                optionSet.insert(sampled);
+            }
+        }
+        optionSet.insert(keyValues[currentIndex]); // Ensure the correct answer is one of the options
+        options.assign(optionSet.begin(), optionSet.end());
     }
-    std::shuffle(options.begin(), options.end(), std::mt19937 { std::random_device {}() });
+
+    std::shuffle(options.begin(), options.end(), gen);
+
+    while (options.size() < 4) {
+        options.emplace_back("", "");
+    }
 
     return std::make_tuple(options[0].second, options[1].second, options[2].second, options[3].second);
 }
